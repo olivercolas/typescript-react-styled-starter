0d791c8ca6ba27f425e6e1117bedd94d3c810011
olivercolas <olivercolas.oc@gmail.com>
2020-04-26 14:52:53 -0400

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Sheet from './components/atoms/Sheet'
+import { ThemeProvider } from 'styled-components'
+import theme from './theme'
+import { Provider } from 'react-redux'
+import store from './store'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Sheet color='secondary' bg='bg.main' alignItems='center' justifyContent='center'>
+          My first project
+        </Sheet>
+      </ThemeProvider>
+    </Provider>
+	)
 }
 
-export default App;
+export default App
